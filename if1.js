d9d35a7ee0fbdd186e634c15f9aee19d7b392176
nguyenthithuuyen <71063276+nguyenthithuuyen@users.noreply.github.com>
2020-09-16 15:11:09 +0700

    Add files via upload

---
+++ if1.js
@@ -0,0 +1,8 @@
+//function testleap(){
+    let value = prompt('What is the "official" name of JavaScript?', '');
+       if (value == 'ECMAScript'){
+           alert('Right!');
+       }else{
+           alert("You donn't know? ECMASCript! ");
+       }
+//}
